5b719c7542696fc19d34189ba4ee6f97eaa5476f
Anthony Ikeda <anthony.ikeda@gmail.com>
2025-07-17 10:56:32 -0700

    Should work this time

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 
+var connectionString = Environment.GetEnvironmentVariable("CUSTOMCONNSTR_AZURE_POSTGRESQL_CONNECTIONSTRING");
+
 if (builder.Environment.IsProduction())
 {
     builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
@@ -52,12 +54,13 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+
 using (var scope = app.Services.CreateScope())
 {
-    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    dbContext.Database.Migrate();
-}
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await db.Database.MigrateAsync();
 
+}
 app.MapControllers();
 app.Run();
 

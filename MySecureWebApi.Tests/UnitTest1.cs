af5660867d9c4f686cbbbad4d974ba7d4c2e4f1d
Anthony Ikeda <anthony.ikeda@gmail.com>
2025-09-28 18:26:26 -0700

    Refactored to accomodate tests

---
+++ MySecureWebApi.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿namespace MySecureWebApi.Tests;
+
+using Ships.DTOs;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var request = new ShipRequestDto
+        {
+            Id = 1,
+            Name = "Gargantuan",
+            Registration = "NCC-5432"
+        };
+        
+        Assert.NotNull(request);
+        
+    }
+}
